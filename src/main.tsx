6642a726810e34d9884fbb1ee31a0c8b7c60decd
宇津　隼人 <hayato.uzu@fusionia.net>
2023-05-13 16:26:17 +0900

    feat: init

---
+++ src/main.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App /> ,
+  },
+  {
+    path: "/user",
+    element:<div>Hello</div>
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
